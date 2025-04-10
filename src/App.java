d12b01eab635a0bd9a7ce139ba01ffc76b9f9743
TheGhxst <elsantiavendano@gmail.com>
2025-04-10 16:41:10 -0500

    Se finaliza el ejercicio

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in)
+        Scanner sc = new Scanner(System.in);
         String NombreEmpleado = "";
         String DocumentoEmpleado = "";
         String DiaDescanso = "";
@@ -25,5 +25,45 @@ public class App {
         DocumentoEmpleado = sc.nextLine();
         System.out.println("Ingrese el día de descanso (solamente de lunes a viernes)");
         DiaDescanso = sc.nextLine();
+        System.out.println("Ingrese el valor de la hora");
+        ValorHora = sc.nextDouble();
+        System.out.println("Ingrese la cantidad de las horas trabajadas");
+        HorasTrabajadas = sc.nextDouble();
+        
+        SalarioBruto = HorasTrabajadas * ValorHora;
+        if (SalarioBruto <= ValorSalarioMinimo * 2) {
+            AuxilioTransporte = 200000;
+        } else {
+            AuxilioTransporte = 0;
+            BonificacionEmpleado = 0;
+
+        }
+        DeduccionPension = 0.04 * SalarioBruto;
+        DeduccionSalud = 0.04 * SalarioBruto;
+
+        if (HorasTrabajadas > 96) {
+            CantidadHorasExtras = HorasTrabajadas - 96;
+            ValorExtra = ValorHora * 1.25;
+            TotalValorHorasExtras = CantidadHorasExtras * ValorExtra;
+            SalarioBruto = SalarioBruto + TotalValorHorasExtras;
+        } else { 
+            CantidadHorasExtras = 0;
+            ValorExtra = 0;
+            TotalValorHorasExtras = 0;
+        }
+
+        if (TotalValorHorasExtras < 24) {
+            System.out.println("El empleado no ha trabajado lo suficiente para recibir salario");
+        } else {
+            SalarioNeto = SalarioBruto - (DeduccionPension + DeduccionSalud) + AuxilioTransporte + BonificacionEmpleado + ValorExtra;
+            System.out.println("El salario bruto del empleado " + NombreEmpleado + " es: " + SalarioBruto);
+            System.out.println("El salario neto del empleado " + NombreEmpleado + " es :" + SalarioNeto);
+            System.out.println("El salario de la deducción de pensión es:" + DeduccionPension);
+            System.out.println("El salario de la deducción pensión es:" + DeduccionSalud);
+            System.out.println("El valor del auxilio de transporte es:" + AuxilioTransporte);
+            System.out.println("El valor de la bonificación del empleado es" + BonificacionEmpleado);
+            System.out.println("El valor de las horas extras es:" + TotalValorHorasExtras);
+            
+        }
     }
 }
